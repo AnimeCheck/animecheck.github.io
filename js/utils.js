ad698eb2b90b11c6048a261d29620cc82debe81a
Rixuel <rixuel@hotmail.com>
2025-08-07 21:03:15 -0400

    fixing timeAgoText to avoid showing 0 months ago

--- js/utils.js
+++ js/utils.js
@@ -121,7 +121,7 @@ function timeAgoText(timestamp) {
     if (days < 7) return `${days} day${days !== 1 ? 's' : ''} ago`;
 
     const weeks = Math.floor(days / 7);
-    if (weeks < 4) return `${weeks} week${weeks !== 1 ? 's' : ''} ago`;
+    if (days < 30) return `${weeks} week${weeks !== 1 ? 's' : ''} ago`;
 
     const months = Math.floor(days / 30);
     if (months < 12) return `${months} month${months !== 1 ? 's' : ''} ago`;
